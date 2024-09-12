eb1f35747e10ebe5809543120515b5ff19d917e8
bijay079 <bijay.pandey@bison.howard.edu>
2024-09-12 18:30:13 -0400

    Second commit

--- LSP_Fall_2024/src/org/howard/edu/lsp/assignment1/Helloworld.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/assignment1/Helloworld.java
@@ -7,7 +7,7 @@ package org.howard.edu.lsp.assignment1;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World LSP!!");
+		System.out.println("Hello World !!");
 
 	}
 
